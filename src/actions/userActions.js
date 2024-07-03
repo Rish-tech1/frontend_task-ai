6fd495279035833cd17a64aa5691fd97c5af1f53
Rishabh Jain <jainrishabh8153@gmail.com>
2024-07-04 01:24:47 +0530

    task

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,12 +1,12 @@
 export const getUsers = () => async dispatch => {
-  try{
+  try {
     const response = await fetch('http://example.com/users')
     const parsedResponse = await response.json()
     dispatch({
       type: 'LIST_USERS',
       payload: parsedResponse
     });
-  }catch(e){
+  }catch (e) {
     console.log(e);
   }
 };
@@ -21,10 +21,43 @@ export const addUser = (payload) => async dispatch => {
       }
     });
     const parsedResponse = await response.json()
-    if(parsedResponse.success){
+    if (parsedResponse.success) {
       dispatch(getUsers());
     }
-  }catch(e){
+  }catch (e) {
     console.log(e);
+  }
+};
+
+export const deleteUser = (id) => async dispatch => {
+  try {
+    const response = await fetch(`http://example.com/user/${id}`, {
+      method: "DELETE",
+    })
+    const parsedResponse = await response.json();
+    if (parsedResponse.success) {
+      dispatch(getUsers());
+    }
+
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export const editUser = (id, newData) => async (dispatch) => {
+  try {
+    const response = await fetch(`http://example.com/user/${id}`, {
+      method: "PUT",
+      body: JSON.stringify(newData),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+    const parsedResponse = await response.json();
+    if (parsedResponse.success) {
+      dispatch(getUsers());
+    }
+  } catch (e) {
+    console.error( e);
   }
 };
